0f05c5e67a5a94363125a76fa400222aac52f8a6
Jean Lima <jcoliveiralima19@gmail.com>
2026-08-19 12:40:33 -0300

    Forçando reiniciliazação do componente de estrelas após uma avaliação

--- src/components/RatingCard/index.jsx
+++ src/components/RatingCard/index.jsx
@@ -7,7 +7,7 @@ export default function RatingCard({ title, year, nextFilm }) {
     <div className="w-75 flex flex-col items-center gap-4 p-8 rounded-2xl bg-gray-600 text-white shadow-[0_5px_15px_rgba(0,0,0,0.35)]">
       <Heading className="font-bold text-xl">{title}</Heading>
       <Heading className="-mt-3 text-sm">{year}</Heading>
-      <StarRating />
+      <StarRating key={title} />
       <ReviewModal />
       <Button type="submit" className="w-full h-10 mt-4 hover:cursor-pointer bg-red-500 text-white rounded-lg" onClick={() => nextFilm()}>Nunca assisti esse filme</Button>
       <Button type="submit" className="w-full h-10 hover:cursor-pointer bg-green-500 text-white rounded-lg" onClick={() => nextFilm()}>Salvar</Button>
